332dd1cc14a739c7e00437adf49419fa3c2d07fb
Liam <liam99mail@proton.me>
2025-02-28 10:31:20 +0100

    Make squares change colour when hovering over them

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 (() => {
-    const grid = document.querySelector('.grid');
+    const gridDiv = document.querySelector('.grid');
     const gridFragment = document.createDocumentFragment();
 
     for (let row = 0; row < 16; row++) {
         const rowDiv = document.createElement('div');
-        rowDiv.classList.add('row')
+        rowDiv.classList.add('row');
 
         for (let col = 0; col < 16; col++) {
             const squareDiv = document.createElement('div');
             squareDiv.classList.add('square');
 
+            squareDiv.addEventListener('mouseover', changeSquareColor);
+
             rowDiv.appendChild(squareDiv);
         }
 
-        grid.appendChild(rowDiv);
+        gridFragment.appendChild(rowDiv);
     }
 
-    grid.appendChild(gridFragment);
+    gridDiv.appendChild(gridFragment);
 })();
+
+
+function changeSquareColor(e) {
+    const squareDiv = e.target;
+    squareDiv.style.backgroundColor = 'black';
+}
